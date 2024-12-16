89d574ee2b35c3661bebecb2672c86d63b803060
Martynas Mickevičius <self@2m.lt>
2024-12-16 18:48:15 +0200

    render plotly client-side

--- src/components/Sodas.tsx
+++ src/components/Sodas.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Plot from 'react-plotly.js';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 
 export default function Sodas() {
     const [data, setData] = useState(null);
@@ -21,45 +21,53 @@ export default function Sodas() {
     if (loading) return <div>Loading...</div>;
 
     return (
-        <Plot
-            data={[{
-                x: unpack(data, 'x'),
-                y: unpack(data, 'z'),
-                z: unpack(data, 'y'),
-                mode: 'markers',
-                marker: {
-                    size: 3,
-                    color: '#317539'
-                },
-                type: 'scatter3d',
-                hoverinfo: 'none',
-            }]}
-            layout={{
-                modebar: {
-                    remove: ['orbitRotation', 'tableRotation', 'resetCameraDefault3d', 'toImage', 'zoom', 'pan']
-                },
-                scene: {
-                    aspectratio: {
-                        x: 1.25,
-                        y: 1.25,
-                        z: 1.25,
-                    },
-                    yaxis: {
-                        title: 'z',
-                    },
-                    zaxis: {
-                        title: 'y',
-                    },
-                },
-                margin: {
-                    l: 0,
-                    r: 0,
-                    b: 0,
-                    t: 0,
-                },
+        // plotly does not support server-side rendering
+        // therefore we need to wrap it in BrowserOnly
+        // so it only runs on the client side
+        <BrowserOnly>
+            {() => {
+                const Plot = require('react-plotly.js').default;
+                return <Plot
+                    data={[{
+                        x: unpack(data, 'x'),
+                        y: unpack(data, 'z'),
+                        z: unpack(data, 'y'),
+                        mode: 'markers',
+                        marker: {
+                            size: 3,
+                            color: '#317539'
+                        },
+                        type: 'scatter3d',
+                        hoverinfo: 'none',
+                    }]}
+                    layout={{
+                        modebar: {
+                            remove: ['orbitRotation', 'tableRotation', 'resetCameraDefault3d', 'toImage', 'zoom', 'pan']
+                        },
+                        scene: {
+                            aspectratio: {
+                                x: 1.25,
+                                y: 1.25,
+                                z: 1.25,
+                            },
+                            yaxis: {
+                                title: 'z',
+                            },
+                            zaxis: {
+                                title: 'y',
+                            },
+                        },
+                        margin: {
+                            l: 0,
+                            r: 0,
+                            b: 0,
+                            t: 0,
+                        },
+                    }}
+                    useResizeHandler={true}
+                    style={{ width: "100%", height: "100%" }}
+                />
             }}
-            useResizeHandler={true}
-            style={{ width: "100%", height: "100%" }}
-        />
+        </BrowserOnly>
     );
 }
